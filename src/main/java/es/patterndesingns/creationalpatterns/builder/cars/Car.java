01646f812b4539a106d1eda80624dce5ba7a3ed0
israel.tobias <25621386+israeltobias@users.noreply.github.com>
2025-02-13 09:46:40 +0100

    Added needed getters/setters

--- src/main/java/es/patterndesingns/creationalpatterns/builder/cars/Car.java
+++ src/main/java/es/patterndesingns/creationalpatterns/builder/cars/Car.java
@@ -45,4 +45,36 @@ public class Car {
         }
         return info;
     }
+
+    public CarType getCarType() {
+        return carType;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public Transmission getTransmission() {
+        return transmission;
+    }
+
+    public TripComputer getTripComputer() {
+        return tripComputer;
+    }
+
+    public GPSNavigator getGpsNavigator() {
+        return gpsNavigator;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
 }
